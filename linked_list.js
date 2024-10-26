404d45d2999bf4501e87b2cfb0047657a8e0896e
TM-Jhur <jhury@davidaperez.com>
2024-10-26 20:51:24 +0900

    change linked list nodes

--- linked_list.js
+++ linked_list.js
@@ -56,8 +56,8 @@ class LinkedList {
 
 // Example usage:
 const list = new LinkedList();
-list.append(1);
-list.append(2);
+list.append(4);
+list.append(5);
 list.append(3);
 list.prepend(0);
 list.delete(2);
